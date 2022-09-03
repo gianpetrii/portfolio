8d030a9439eff74deee9dc2705e6d57c940c4fb3
Gianluca <gianpetrii@gmail.com>
2022-09-03 20:37:44 -0300

    swiper for testimonials done

--- src/sections/Testimonials/Testimonials.jsx
+++ src/sections/Testimonials/Testimonials.jsx
@@ -1,31 +1,79 @@
+// react
+import { Swiper, SwiperSlide } from "swiper/react";
+import { useState, useEffect } from "react";
+import React from "react";
+
 // Estilos
 import './Testimonials.css';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
 
 // Files
 import AVATAR1 from '../../assets/avatar1.jpg';
 import AVATAR2 from '../../assets/avatar2.jpg';
 import AVATAR3 from '../../assets/avatar3.jpg';
 import AVATAR4 from '../../assets/avatar4.jpg';
+import { Autoplay, Pagination, Navigation } from "swiper";
+
 
 function Testimonials() {
+
+   // handle taking out navigation in slider when tablet or mobile display
+   const [isSmallerThanTablet, setIsSmallerThanTablet] = useState(true);
+
+   useEffect(() => {
+      function handleResize() {
+        if (window.innerWidth < 1024) {
+          setIsSmallerThanTablet(false);
+        } else {
+          setIsSmallerThanTablet(true);
+        }
+      }
+  
+      handleResize();
+  
+      window.addEventListener("resize", handleResize);
+  
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+
+
+
+
    return (
       <section id='testimonials'>
          <h5>Review from clients</h5>
          <h2>Testimonials</h2>
 
-         <div className="testimonials-container container">
+         <Swiper
+               spaceBetween={30}
+               centeredSlides={true}
+               autoplay={{
+                  delay: 2500,
+                  disableOnInteraction: false,
+               }}
+               pagination={{
+                  clickable: true,
+               }}
+               navigation={isSmallerThanTablet}
+               modules={[Autoplay, Pagination, Navigation]}
+               className="testimonials-container container"
+         >
+               
+               {testimonials.map(testimonial => (
+                  <SwiperSlide className='testimonials-article'>
+                     <div className='testimonials-avatar'>
+                        <img src={testimonial.image} alt="avatar" />
+                     </div>
+                     <h5>{testimonial.name}</h5>
+                     <small className='testimonials-review'>{`" ${testimonial.content} "`}</small>
+                  </SwiperSlide>           
+                  ))}
 
-            {testimonials.map(testimonial => (
-               <article className='testimonials-article'>
-                  <div className='testimonials-avatar'>
-                     <img src={testimonial.image} alt="avatar" />
-                  </div>
-                  <h5>{testimonial.name}</h5>
-                  <small className='testimonials-review'>{`" ${testimonial.content} "`}</small>
-               </article>              
-            ))}
-
-         </div>
+         </Swiper>
       </section>
    );
 }
