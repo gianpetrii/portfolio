6ef4688348ad90c1db47029c9e67281f965f810e
Gianluca <gianpetrii@gmail.com>
2022-09-04 10:54:04 -0300

    projects slider for mobile and tablets

--- src/sections/Portfolio/Portfolio.jsx
+++ src/sections/Portfolio/Portfolio.jsx
@@ -1,5 +1,14 @@
+// react
+import { Swiper, SwiperSlide } from 'swiper/react';
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+
 // Estilos
 import './Portfolio.css';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 // files
 import IMG1 from '../../assets/portfolio1.jpg';
@@ -8,31 +17,92 @@ import IMG3 from '../../assets/portfolio1.jpg';
 import IMG4 from '../../assets/portfolio1.jpg';
 import IMG5 from '../../assets/portfolio1.jpg';
 import IMG6 from '../../assets/portfolio1.jpg';
+import { Autoplay, Pagination, Navigation } from "swiper";
+
 
 function Portfolio() {
-    return (
+   // handle taking out navigation in slider when tablet or mobile display
+   const [isSmallerThanTablet, setIsSmallerThanTablet] = useState(false);
+   
+   useEffect(() => {
+      function handleResize() {
+        if (window.innerWidth < 1250) {
+          setIsSmallerThanTablet(true);
+        } else {
+          setIsSmallerThanTablet(false);
+        }
+      }
+  
+      handleResize();
+  
+      window.addEventListener("resize", handleResize);
+  
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
+   
+   
+   return (
       <section id='portfolio'>
          <h5>My best ventures</h5>
          <h2>Projects and Developments</h2>
 
-         <div className="portfolio-container container">
+         {isSmallerThanTablet ? 
+            <>
+               <Swiper
+                  spaceBetween={30}
+                  centeredSlides={true}
+                  autoplay={{
+                     delay: 2500,
+                     disableOnInteraction: false,
+                  }}
+                  pagination={{
+                     clickable: true,
+                  }}
+                  navigation={false}
+                  modules={[Autoplay, Pagination, Navigation]}
+                  className="portfolio-container container"
+               >
 
-            {projects.map(project => (
-               <article className='portfolio-article'>   {/* AGREGAR QUE PARA PHONE SE PASE PARA EL COSTADO */}
-                  <div className="portfolio-div-image">
-                     <img src={project.image} />{/* arreglar tema de ruta imagen */}
-                  </div>
-                  <h3>{project.title}</h3>
+                  {projects.map(project => (
+                     <SwiperSlide className='portfolio-article'>   {/* AGREGAR QUE PARA PHONE SE PASE PARA EL COSTADO */}
+                        <div className="portfolio-div-image">
+                           <img src={project.image} />{/* arreglar tema de ruta imagen */}
+                        </div>
+                        <h3>{project.title}</h3>
 
-                  <div className="portfolio-buttons">
-                     <a href={project.github} className='btn' target="_blank">GitHub</a>
-                     <a href={project.demo} className='btn btn-primary' target="_blank">Live Demo</a>
-                  </div>
+                        <div className="portfolio-buttons">
+                           <a href={project.github} className='btn' target="_blank">GitHub</a>
+                           <a href={project.demo} className='btn btn-primary' target="_blank">Live Demo</a>
+                        </div>
 
-               </article>
-            ))}
+                     </SwiperSlide>
+                  ))}
 
-         </div>
+               </Swiper>
+
+            </> : <>
+
+               <div className="portfolio-container container">
+
+                  {projects.map(project => (
+                     <article className='portfolio-article'>   {/* AGREGAR QUE PARA PHONE SE PASE PARA EL COSTADO */}
+                        <div className="portfolio-div-image">
+                           <img src={project.image} />{/* arreglar tema de ruta imagen */}
+                        </div>
+                        <h3>{project.title}</h3>
+
+                        <div className="portfolio-buttons">
+                           <a href={project.github} className='btn' target="_blank">GitHub</a>
+                           <a href={project.demo} className='btn btn-primary' target="_blank">Live Demo</a>
+                        </div>
+
+                     </article>
+                  ))}
+
+               </div>  
+            </>
+         }
+
       </section>
     );
 }
