41e9f7564ecbb05a2c730c2fd83a073da28f3398
Gianluca <gianpetrii@gmail.com>
2022-09-05 11:46:24 -0300

    service shows more info when clicked

--- src/sections/Services/Services.jsx
+++ src/sections/Services/Services.jsx
@@ -1,8 +1,47 @@
+// react
+import React, { useState, useEffect } from 'react';
+
 // Estilos
 import './Services.css';
 
 function Services() {
-    return (
+
+   //SWIPER WHEN DISPLAY IS TABLET OR SMALLER
+   const [isSmallerThanTablet, setIsSmallerThanTablet] = useState("non-active");
+   
+   useEffect(() => {
+      function handleResize() {
+        if (window.innerWidth < 1250) {
+          setIsSmallerThanTablet("active");
+        } else {
+          setIsSmallerThanTablet("non-active");
+        }
+      }
+  
+      handleResize();
+  
+      window.addEventListener("resize", handleResize);
+  
+      return () => window.removeEventListener("resize", handleResize);
+   }, []);
+
+
+
+
+   
+   // SET ACTIVE SERVICE
+   const [activeService, setActiveService] = useState(0);
+
+   const handleMoreInfo = (id) => {
+       if (activeService == id) {
+           setActiveService(0);
+       } else {
+           setActiveService(id);
+       }
+   };
+  
+
+   return (
       <section id='services'>
          <h5>What I Offer</h5>
          <h2>Services</h2>
@@ -10,18 +49,23 @@ function Services() {
          <div className="services-container container">
 
             {servicios.map(servicio => (
-               <article className="service">
-                  <div className="service-head">
+               <article className="service-article" key={servicio.id}>
+                  <div className="service-head" onClick={() => handleMoreInfo(`${servicio.id}`)}>
                      <h3>{servicio.title}</h3>
                   </div>
-   
-                  <ul className='service-list'>
+
+                  {activeService === `${servicio.id}` && (
+
+                     <ul className='service-list'>
                      {servicio.services.map(service => (
                         <li>
                            <p>{service.content}</p>
                         </li>
                      ))}
-                  </ul>
+                     </ul>
+
+                  )}
+
    
                </article>
             ))}
@@ -35,6 +79,7 @@ function Services() {
 export default Services;
 
 const servicios = [{
+   id: 1,
    title: "Frontend Development",
    services: [{
       content: "- Este seria sdfsdfdsuno de los servicios"
@@ -50,6 +95,7 @@ const servicios = [{
       content: "- Este seria sdfsdfdsuno de los servicios"
       }]
    }, {
+      id: 2,
       title: "Backend Development",
       services: [{
          content: "- Este seria sdfsdfdsuno de los servicios"
@@ -65,6 +111,7 @@ const servicios = [{
          content: "- Este seria sdfsdfdsuno de los servicios"
          }]
    }, {
+      id: 3,
       title: "Python Task Automation",
       services: [{
          content: "- Este seria sdfsdfdsuno de los servicios"
